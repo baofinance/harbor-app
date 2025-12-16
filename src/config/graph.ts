022d111982652241c4bbddfbe80b21f7c245b417
Chickn <chicknbao@protonmail.com>
2025-12-16 22:08:49 +0000

    Update subgraph endpoint to v0.0.9 (current staging contracts only)

--- src/config/graph.ts
+++ src/config/graph.ts
@@ -5,7 +5,7 @@ export const GRAPH_CONFIG = {
   marks: {
     url:
       process.env.NEXT_PUBLIC_GRAPH_URL ||
-      "https://api.studio.thegraph.com/query/1718836/harbor-marks/v0.0.6",
+      "https://api.studio.thegraph.com/query/1718836/harbor-marks/v0.0.9",
     chainId: 1,
     network: "mainnet",
   },
@@ -21,7 +21,7 @@ export const GRAPH_CONFIG = {
   production: {
     url:
       process.env.NEXT_PUBLIC_GRAPH_URL ||
-      "https://api.studio.thegraph.com/query/1718836/harbor-marks/v0.0.6",
+      "https://api.studio.thegraph.com/query/1718836/harbor-marks/v0.0.9",
     chainId: 1,
     network: "mainnet",
   },
